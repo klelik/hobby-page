15c9019715ef2654ac3596df7fa89d3ed55614ef
klelik <klelika1998@gmail.com>
2023-01-21 21:22:21 +0000

    Hobby page V.2

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const dateElement = document.getElementById("date");
+var slide = document.getElementById("slide");
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+var cardNumber = document.getElementsByClassName("card"); //how many cards we have
+var cardHeight = document.querySelector(".card").offsetHeight;  //getting the height of the card
 
-let today = new Date();
-let day = today.getDate();
-let month = today.getMonth() + 1;
-let year = today.getFullYear();
+let x = 0;
 
-dateElement.textContent = `Today's date is ${day}/${month}/${year}`;
+prev.onclick = function () {
+  if (x > -(cardNumber.length -1)*cardHeight) {   //-1 because we are already displaying the first one 
+    //300 the height off the card * how many cards are make this dynamic later
+    x = x - cardHeight;
+    slide.style.top = x + "px";
+    console.log(cardHeight);
+  }
+};
+next.onclick = function () {
+  if (x < "0") {
+    //300 the height off the card * how many cards are make this dynamic later
+    x = x + cardHeight;
+    slide.style.top = x + "px";
+    console.log(cardHeight);
+  }
+};
